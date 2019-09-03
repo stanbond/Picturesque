502b047eb096f79a529eb731855f22ce97e3a91a
StanBond <stan.bond@ymail.com>
2019-09-03 11:17:07 -0600

    user backend initial commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,23 @@
 class SessionsController < ApplicationController
   def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+    if @user
+      login(@user)
+      render 'api/users/show'
+    else
+      render json: ['Wrong credentials!'], status: 401
+    end
   end
-
-  def new
-  end
-
+  
   def destroy
+    if current_user
+      logout
+      render json: ['Logout Success']
+    else
+      render json: {}, status: 404
+    end
   end
 end
