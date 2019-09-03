8b3f1d4cab9ed0158df1f5959d44fc72b8b468d9
StanBond <stan.bond@ymail.com>
2019-09-03 12:28:32 -0600

    user backend user/session moved to api folder

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,4 +1,4 @@
-class SessionsController < ApplicationController
+class API::SessionsController < ApplicationController
   def create
     @user = User.find_by_credentials(
       params[:user][:username],
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string
+#  password_digest :string
+#  session_token   :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
 class User < ApplicationRecord
   validates :username, :password_digest, :session_token, presence: true
   validates :password, length: { minimum: 6, allow_nil: true}
