64076bba6d06d50466cbb51b37275d7c8e4e7436
StanBond <stan.bond@ymail.com>
2019-09-09 12:56:44 -0600

    created/migrated posts

---
+++ app/controllers/api/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+  before_action :ensure_login
+
+  def create
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  def index
+    
+  end
+
+  def show
+
+  end
+
+end
---
+++ app/models/post.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  user_id    :integer          not null
+#  caption    :text             not null
+#  location   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Post < ApplicationRecord
+  validates :user_id, :caption, presence: true
+  validate :ensure_attachment
+  
+  has_one_attached :photo
+  belongs_to :user
+  # has_many :likes
+  # has_many :comments 
+
+  def ensure_attachment
+    unless self.photo.attached?
+      errors[:post] << "Must include photo"
+    end
+  end
+end
