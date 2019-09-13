738e13ca5be26d283f85449d14208b0c444a922b
StanBond <stan.bond@ymail.com>
2019-09-13 10:00:15 -0600

    fixed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
+Post.destroy_all
 User.create(email:"batman@picturesque.com", fullname:"Bruce Wayne", username:"bruce_wayne", password: "password")
