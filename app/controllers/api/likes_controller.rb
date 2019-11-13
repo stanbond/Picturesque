f51445657b033ab9d231cf941d82e1dfa3a7b77c
StanBond <stan.bond@ymail.com>
2019-11-13 11:52:01 -0700

    likes controller complete

--- app/controllers/api/likes_controller.rb
+++ app/controllers/api/likes_controller.rb
@@ -1,2 +1,26 @@
-class LikesController < ApplicationController
+class Api::LikesController < ApplicationController
+  before_action :require_logged_in
+
+  def create
+    @like = Like.new(like_params)
+    @like.user_id = current_user.id
+      if @like.save
+        render :show
+      else
+        render json: @like.errors.full_messages, status: 418
+      end
+  end
+
+  def destroy
+    @like = Like.find_by_id(params[:id])
+      if @like && @like.user_id == current_user.id && @like.destroy 
+        render json: {}, status: 200
+      end
+  end
+
+  private
+
+  def like_params
+    params.require(:like).permit(:post_id)
+  end
 end
